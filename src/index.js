333e59c5f6cc67ca2f2f0b91dd48fdd12f396600
jonkkel <79365635+jonkkel@users.noreply.github.com>
2021-09-16 23:01:14 +0300

    T5 done

--- src/index.js
+++ src/index.js
@@ -9,18 +9,42 @@ addCommentButton.addEventListener("click", function () {
   const textArea = document.getElementById("comment");
   const list = document.getElementById("list");
   let li = document.createElement("li");
-  li.innerHTML = textArea.value;
+
+  var rating = document.getElementById("rating");
+  var text = rating.options[rating.selectedIndex].text;
+
+  let comment = document.createElement("div");
+  let comment_rating = document.createElement("div");
+  let comment_text = document.createElement("div");
+  const button = document.createElement("button");
+  button.innerHTML = "remove";
+  button.classList.add("remove-comment");
+  button.style.visibility = "hidden";
+  button.onclick = function () {
+    this.closest("li").remove();
+  };
+
+  comment_rating.classList.add("comment-rating");
+  comment_text.classList.add("comment_text");
+
+  comment_rating.innerHTML = text;
+  comment_text.innerHTML = textArea.value;
+
+  comment.appendChild(comment_rating);
+  comment.appendChild(comment_text);
+  comment.appendChild(button);
+
+  li.appendChild(comment);
+  textArea.value = "";
   list.appendChild(li);
 });
 
 const deleteCommentButton = document.getElementById("delete-comment");
 deleteCommentButton.addEventListener("click", function () {
-  const list = document.getElementById("list");
-  var r = window.confirm("Delete all comments?");
-  if (r === true) {
-    while (list.firstChild) {
-      list.removeChild(list.lastChild);
-    }
-  } else {
+  const elems = document.getElementsByClassName("remove-comment");
+  let i = 0;
+  while (elems[i]) {
+    elems[i].style.visibility = "visible";
+    i++;
   }
 });
